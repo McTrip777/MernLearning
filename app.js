3c70b5f4e09d0d3ad0ac8e1ebb185ce6bb521797
Jacob McFaul <jacob.m.mcfaul@gmail.com>
2021-07-21 14:51:49 -0600

    Added temp Post route

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ const placesRoutes = require('./routes/places-routes')
 
 const app = express()
 
-app.use('/api/places',placesRoutes)
+app.use(bodyParser.json())
 
+app.use('/api/places', placesRoutes)
+
+
+app.use((err, req, res, next) => {
+    if (res.headerSent) {
+        return next(err);
+    }
+
+    res.status(err.code || 500);
+    res.json({ message: err.message || "An unknown error occurred" })
+})
 
 app.listen(5000)
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,46 +1,13 @@
 const express = require('express');
 
+const placesContollers = require('../controllers/places-controller')
+
 const router = express.Router();
 
-const DUMMY_PLACES = [
-    {
-        id:'p1',
-        title: "Empire State Building",
-        description: "One of the most famous sky scrapers in the world",
-        location: {
-            lat: 40.7484474,
-            lng: -73.9871516
-        },
-        address: '20 W 34th St, New York, NY 10001',
-        creator: 'u1'
-    },
-    {
-        id:'p2',
-        title: "Empire State Building",
-        description: "One of the most famous sky scrapers in the world",
-        location: {
-            lat: 40.7484474,
-            lng: -73.9871516
-        },
-        address: '20 W 34th St, New York, NY 10001',
-        creator: 'u1'
-    },
-]
+router.get('/:pid', placesContollers.getPlaceById)
 
-router.get('/:pid', (req, res, next) => {
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p => p.id === placeId) 
-    res.json({place})
-})
+router.get('/user/:uid', placesContollers.getPlaceByUserId)
 
-router.get('/user/:uid', (req, res, next) => {
-    const userId = req.params.uid;
-    const userPlaceList = DUMMY_PLACES.map(p => {
-        if(p.creator === userId){
-            return p;
-        }
-    }) 
-    res.json(userPlaceList)
-})
+router.post('/', placesContollers.createPlace)
 
 module.exports = router;

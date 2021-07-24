822a697c46a76db46e3aa61b80f92050b3f3b125
Jacob McFaul <jacob.m.mcfaul@gmail.com>
2021-07-23 22:57:13 -0600

    Basic backend setup and routes. Need database to start connecting.

--- node-express-backend/app.js
+++ node-express-backend/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
+require('dotenv').config()
 const bodyParser = require('body-parser')
+const HttpError = require('./models/http-error')
 
 const placesRoutes = require('./routes/places-routes')
+const usersRoutes = require('./routes/users-routes')
 
 const app = express()
 
 app.use(bodyParser.json())
 
+app.use('/api/users', usersRoutes)
 app.use('/api/places', placesRoutes)
 
+app.use((req, res, next) => {
+    const err = new HttpError("Could not find this route.", 404)
+    throw err
+})
 
 app.use((err, req, res, next) => {
     if (res.headerSent) {

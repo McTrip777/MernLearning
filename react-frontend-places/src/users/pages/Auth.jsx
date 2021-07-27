d3e643af389ec1e9d0b6f13c0ce37a6c2d6a4659
Jacob McFaul <jacob.m.mcfaul@gmail.com>
2021-07-27 17:48:26 -0600

    Signup is working but the error isn't returning the message it should

--- react-frontend-places/src/users/pages/Auth.jsx
+++ react-frontend-places/src/users/pages/Auth.jsx
@@ -7,15 +7,19 @@ import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from "../../shared/util/validators";
+import ErrorModal from "../../shared/components/UIElements/jsx/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/jsx/LoadingSpinner";
 import Card from "../../shared/components/UIElements/jsx/Card";
 import { AuthContext } from "../../shared/context/auth-context";
-import axios from 'axios'
+import axios from "axios";
 
 import "./Auth.scss";
 
 const Auth = () => {
   const auth = useContext(AuthContext);
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
   const [formState, inputHandler, setFormData, defaultSubmit] = useForm(
     {
       email: {
@@ -55,100 +59,116 @@ const Auth = () => {
     setIsLogin((prevMode) => !prevMode);
   };
 
-  const authSubmitHandler = async event => {
-      event.preventDefault()
+  const authSubmitHandler = async (event) => {
+    event.preventDefault();
 
-      if(isLogin){
-
-      }else{
-        try {
-          const response = await fetch('http://localhost:5000/api/users/signup', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
+    setIsLoading(true);
+    if (isLogin) {
+    } else {
+      try {
+        await axios
+          .post(
+            `http://localhost:5000/api/users/signup`,
+            {
               name: formState.inputs.name.value,
               email: formState.inputs.email.value,
-              password: formState.inputs.password.value
-            })
+              password: formState.inputs.password.value,
+            },
+            {
+              headers: { "Content-Type": "application/json" },
+            }
+          )
+          .then((res) => {
+            console.log(res);
+            setIsLoading(false);
+            auth.login();
           })
-            const responseData = response.json()
-            console.log(responseData)
-        } catch (error) {
-          console.log(error)
-        }
-        
+          .catch((err) => {
+            console.log(err);
+            setIsLoading(false);
+            setError(err.message || "Something went wrong, please try again.");
+          });
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message);
       }
-
-      // await axios.post(`http://localhost:5000/api/users/signup`, {
-      //   body: {
-      //     name: formState.inputs.name.value,
-      //     email: formState.inputs.email.value,
-      //     password: formState.inputs.password.value
-      //   }
-      // },
-      // {
-      //   headers: {'Content-Type': 'application/json'}
-      // }).then(res => {
-      //   console.log(res.data)
-      //   const responseData = res.json()
-      //   console.log(responseData)
-      // }).catch(error => {
-      //   console.log(error)
-      // })
-      auth.login()
-  }
+    }
+  };
+  const errorHandler = () => {
+    setError(null);
+  };
 
   return (
-    <Card className="authentication">
-      <h2>{isLogin ? "Login Required" : "Create an Account"}</h2>
-      <hr />
-      <form className="place-form" onSubmit={authSubmitHandler}>
-        {!isLogin && (
+    <>
+      <ErrorModal error={error} onClear={errorHandler} />
+      <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverlay />}
+        <h2>{isLogin ? "Login Required" : "Create an Account"}</h2>
+        <hr />
+        <form className="place-form" onSubmit={authSubmitHandler}>
+          {!isLogin && (
+            <Input
+              id="name"
+              element="input"
+              type="text"
+              label="Your Name"
+              validators={[VALIDATOR_REQUIRE()]}
+              onInput={inputHandler}
+              // value={formState.inputs.email.value}
+              // valid={formState.inputs.email.isValid}
+              errorText="Please enter a your name"
+            ></Input>
+          )}
           <Input
-            id="name"
+            id="email"
             element="input"
-            type="text"
-            label="Your Name"
-            validators={[VALIDATOR_REQUIRE()]}
+            type="email"
+            label="Email"
+            validators={[VALIDATOR_EMAIL()]}
             onInput={inputHandler}
-            // value={formState.inputs.email.value}
-            // valid={formState.inputs.email.isValid}
-            errorText="Please enter a your name"
+            value={formState.inputs.email.value}
+            valid={formState.inputs.email.isValid}
+            errorText="Please enter a valid email"
           ></Input>
-        )}
-        <Input
-          id="email"
-          element="input"
-          type="email"
-          label="Email"
-          validators={[VALIDATOR_EMAIL()]}
-          onInput={inputHandler}
-          value={formState.inputs.email.value}
-          valid={formState.inputs.email.isValid}
-          errorText="Please enter a valid email"
-        ></Input>
-        <Input
-          id="password"
-          element="input"
-          type="password"
-          label="Password"
-          validators={[VALIDATOR_MINLENGTH(5)]}
-          onInput={inputHandler}
-          value={formState.inputs.password.value}
-          valid={formState.inputs.password.isValid}
-          errorText="Please enter at least 5 characters"
-        ></Input>
-        <Button type="submit" disabled={!formState.isValid}>
-          {isLogin ? "Login" : "Sign Up"}
+          <Input
+            id="password"
+            element="input"
+            type="password"
+            label="Password"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            onInput={inputHandler}
+            value={formState.inputs.password.value}
+            valid={formState.inputs.password.isValid}
+            errorText="Please enter at least 5 characters"
+          ></Input>
+          <Button type="submit" disabled={!formState.isValid}>
+            {isLogin ? "Login" : "Sign Up"}
+          </Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>
+          Switch to {isLogin ? "Sign Up" : "Login"}
         </Button>
-      </form>
-      <Button inverse onClick={switchModeHandler}>
-        Switch to {isLogin ? "Sign Up" : "Login"}
-      </Button>
-    </Card>
+      </Card>
+    </>
   );
 };
 
 export default Auth;
+
+// try {
+//   const response = await fetch('http://localhost:5000/api/users/signup', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       name: formState.inputs.name.value,
+//       email: formState.inputs.email.value,
+//       password: formState.inputs.password.value
+//     })
+//   })
+//     const responseData = response.json()
+//     console.log(responseData)
+// } catch (error) {
+//   console.log(error)
+// }
